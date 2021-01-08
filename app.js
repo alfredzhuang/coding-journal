fbaa993a39db0746f7f7bf2f52751e1c14b4fdb6
alfredzhuang <alfredzhuang123@gmail.com>
2021-01-08 15:03:15 -0800

    setup login/register functionality

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
+require("dotenv").config();
 let express = require("express");
 let bodyParser = require("body-parser");
 let ejs = require("ejs");
+let session = require("express-session");
+let passport = require("passport");
+let passportLocalMongoose = require("passport-local-mongoose");
 
-let userSchema = require("./database.js");
+let { mongoose, userSchema } = require("./database.js");
 let app = express();
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({secret: process.env.SECRET, resave: false, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
 
+userSchema.plugin(passportLocalMongoose);
+let User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(req, res) {
-    res.render("home");
+    if(req.isAuthenticated()) {
+        res.redirect("/browse");
+    } else {
+        res.render("home");
+    }
 });
 
 app.get("/login", function(req, res) {
@@ -23,7 +40,11 @@ app.get("/register", function(req, res) {
 });
 
 app.get("/browse", function(req, res) {
-    res.render("browse");
+    if(req.isAuthenticated()) {
+        res.render("browse");
+    } else {
+        res.redirect("/");
+    }
 });
 
 app.get("/profile", function(req, res) {
@@ -32,6 +53,35 @@ app.get("/profile", function(req, res) {
 
 app.get("/create", function(req, res) {
     res.render("create");
+});
+
+app.post("/register", function(req, res) {
+    User.register({username: req.body.username}, req.body.password, function(err, user) {
+        if(err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/browse");
+            }) 
+        }
+    })
+});
+
+app.post("/login", function(req, res) {
+    let user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/browse");
+            });
+        }
+    });
 });
 
 app.listen(3000, function() {
--- database.js
+++ database.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 let mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB, {useNewUrlPaerser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB, {useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex", true);
 
 let userSchema = new mongoose.Schema({
@@ -8,4 +9,4 @@ let userSchema = new mongoose.Schema({
     password: String,
 })
 
-module.exports = userSchema;
+module.exports = { mongoose, userSchema };
