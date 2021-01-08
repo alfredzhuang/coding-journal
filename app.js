88a139838eb1925e2241106797e408a73f5a1384
alfredzhuang <alfredzhuang123@gmail.com>
2021-01-08 13:45:37 -0800

    setup page routes

--- app.js
+++ app.js
@@ -8,6 +8,30 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/", function(req, res) {
+    res.render("home");
+});
+
+app.get("/login", function(req, res) {
+    res.render("login");
+});
+
+app.get("/register", function(req, res) {
+    res.render("register");
+});
+
+app.get("/browse", function(req, res) {
+    res.render("browse");
+});
+
+app.get("/profile", function(req, res) {
+    res.render("profile");
+});
+
+app.get("/create", function(req, res) {
+    res.render("create");
+});
+
 app.listen(3000, function() {
     console.log("Server started on port 3000");
 })
